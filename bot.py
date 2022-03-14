62c73291ed2fad7c344db5282a5a5815d157c7d2
Pablo Mateos Ruiz <pablomateosruiz@gmail.com>
2022-03-14 20:14:46 +0100

    first test with database

--- bot.py
+++ bot.py
@@ -1,9 +1,29 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
-PORT = int(os.environ.get('PORT', '8443'))
-TOKEN = os.environ["TOKEN"]
+from os import environ
+import firebase_admin
+from firebase_admin import db
+import json
+
+PORT = int(environ.get('PORT', '8443'))
+TOKEN = environ["TOKEN"]
 NAME = 'benaluma-bot'
+
+ENV_KEYS = {
+    "type": "service_account",
+    "project_id": environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+    "client_email": environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": environ["FIREBASE_CLIENT_ID"],
+    "token_uri": environ["FIREBASE_TOKEN_URI"],
+}
+
+# Setup Firebase database
+firebase_admin.initialize_app(
+    credentials.Certificate(ENV_KEYS),
+    {'databaseURL': environ["FIREBASE_DATABASE_URL"]}
+)
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -26,6 +46,11 @@ def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
+def record(update, context):
+    """Logs message into database."""
+    ref = db.reference(str(update.effective_chat.id))
+    ref.set({"message": update.message.text})
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -43,6 +68,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("record", record))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
